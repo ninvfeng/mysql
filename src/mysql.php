b15a43a68013e1d422a4caa7ab2be8e64aacc533
冯誉信 <ninvfeng@qq.com>
2018-06-04 16:18:14 +0800

    修改查询参数传递方式, 防sql注入更安全 当只查一个字段时默认返回一维数组, 使用更方便

--- src/mysql.php
+++ src/mysql.php
@@ -10,14 +10,22 @@ class mysql
     protected $_limit='';
     protected $_join='';
     protected $_debug=false;
+    protected $_param=[];
 
-    function __construct($config)
+    function __construct($config=[])
     {
+        if(!$config){
+            $config=config('mysql');
+        }
+
         //链接数据库
         $this->_pdo=new \PDO('mysql:host='.$config['host'].';dbname='.$config['name'],$config['user'],$config['pass'],array(\PDO::ATTR_PERSISTENT => true));
 
         //设置客户端字符集
         $this->_pdo->exec("set names 'utf8'");
+
+        //禁用prepared statements的仿真效果 确保SQL语句和相应的值在传递到mysql服务器之前是不会被PHP解析
+        $this->_pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
 
         //数据表
         $this->_table=$table;
@@ -55,6 +63,7 @@ class mysql
     //条件
     public function where($where){
         if(is_array($where)){
+            $this->_param=$where;
             $res='';
             foreach($where as $k => $v){
                 $column_key='';
@@ -62,7 +71,7 @@ class mysql
                     $column_key.='`'.$vv.'`.';
                 }
                 $column_key=trim($column_key,'.');
-                $res.=$column_key.'="'.$v.'" and';
+                $res.=$column_key.'=:'.$k.' and';
             }
             $where=trim($res,'and');
         }
@@ -98,7 +107,15 @@ class mysql
 
     //结果集
     public function select(){
-        return $this->_query();
+        $res=$this->_query();
+        if(count($res[0])==1){
+            $column=explode('.',$this->_field);
+            $column=array_pop($column);
+            $result=array_column($res,$column);
+            return $result;
+        }else{
+            return $res;
+        }
     }
 
     //获取单条数据
@@ -109,6 +126,7 @@ class mysql
     //更新
     public function update($data){
         if($this->_where){
+            $this->_param=array_merge($this->_param,$data);
             $update='';
             foreach($data as $k => $v){
                 $column_key='';
@@ -116,20 +134,20 @@ class mysql
                     $column_key.='`'.$vv.'`.';
                 }
                 $column_key=trim($column_key,'.');
-                $update.=column_key."='".$v."',";
+                $update.=$column_key."=:".$k.",";
             }
             $update=trim($update,',');
             $sql="update {$this->_table} set $update {$this->_where};";
-            return $this->_exec($sql);
+            return $this->exec($sql,$this->_param);
         }else{
-            error('保存数据需指定条件');
+            echo '保存数据需指定条件';
+            die();
         }
-
-
     }
 
     //添加
     public function insert($data){
+        $this->_param=array_merge($this->_param,$data);
         $update='';
         foreach($data as $k => $v){
             $column_key='';
@@ -137,11 +155,11 @@ class mysql
                 $column_key.='`'.$vv.'`.';
             }
             $column_key=trim($column_key,'.');
-            $update.=column_key."='".$v."',";
+            $update.=$column_key."=:".$k.",";
         }
         $update=trim($update,',');
         $sql="insert into {$this->_table} set $update;";
-        $this->_exec($sql);
+        $this->exec($sql,$this->_param);
         return $this->_pdo->lastInsertId();
     }
 
@@ -149,33 +167,44 @@ class mysql
     public function delete(){
         if($this->_where){
             $sql="delete from {$this->_table} {$this->_where};";
-            return $this->_exec($sql);
+            return $this->exec($sql,$this->_param);
         }else{
-            error('删除数据需指定条件');
+            echo '删除数据需指定条件';
+            die();
         }
     }
 
     //执行原生query
     public function query($sql,$param=[]){
-        //禁用prepared statements的仿真效果 确保SQL语句和相应的值在传递到mysql服务器之前是不会被PHP解析
-        $this->_pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
-
-        $pre=$this->_pdo->prepare($sql);
-        $pre->execute($param);
-        if($this->_error()){
-            return $pre->fetchAll(\PDO::FETCH_ASSOC);
+        if($this->_debug){
+            echo "<pre>";
+            echo $sql;
+            echo "<br>";
+            print_r($param);
+            die();
+        }else{
+            $pre=$this->_pdo->prepare($sql);
+            $pre->execute($param);
+            if($this->_error()){
+                return $pre->fetchAll(\PDO::FETCH_ASSOC);
+            }
         }
     }
 
     //执行原生exec
     public function exec($sql,$param=[]){
-        //禁用prepared statements的仿真效果 确保SQL语句和相应的值在传递到mysql服务器之前是不会被PHP解析
-        $this->_pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
-
-        $pre=$this->_pdo->prepare($sql);
-        $res=$pre->execute($param);
-        if($this->_error()){
-            return $res;
+        if($this->_debug){
+            echo "<pre>";
+            echo $sql;
+            echo "<br>";
+            print_r($param);
+            die();
+        }else{
+            $pre=$this->_pdo->prepare($sql);
+            $res=$pre->execute($param);
+            if($this->_error()){
+                return $res;
+            }
         }
     }
 
@@ -196,31 +225,10 @@ class mysql
         }
     }
 
-    //执行
-    protected function _exec($sql){
-        if($this->_debug){
-            echo $sql;
-            die();
-        }else{
-            $res=$this->_pdo->exec($sql);
-            if($this->_error()){
-                return $res;
-            }
-        }
-    }
-
     //查询
     protected function _query(){
         $sql="select {$this->_field} from {$this->_table} {$this->_join} {$this->_where} {$this->_order} {$this->_limit}";
-        if($this->_debug){
-            echo $sql;
-            die();
-        }else{
-            $res=$this->_pdo->query($sql);
-            if($this->_error()){
-                return $res->fetchAll(\PDO::FETCH_ASSOC);
-            }
-        }
+        return $this->query($sql,$this->_param);
     }
 
     //错误处理
